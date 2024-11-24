7a788b3f4b0e462e399b9e3aa8ffd7f28550d2b4
ebini <ebini@student.42lyon.fr>
2024-11-24 12:14:08 +0100

    fix contants.h -> constants.h

---
+++ constants.h
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   constants.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebini <ebini@student.42lyon.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/23 17:58:26 by ebini             #+#    #+#             */
+/*   Updated: 2024/11/24 11:16:39 by ebini            ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONSTANTS_H
+# define CONSTANTS_H
+
+// MAX VALUES
+
+# define MAX_PLAYER_MISSILE 100
+# define MAX_ASTEROIDS 42
+
+// SPEED VALUES
+
+#define	ASTEROID_SPEED 20
+#define	MISSILE_SPEED 5
+
+// MAP VALUES
+
+# define MAP_WIDTH 60
+# define MAP_HEIGHT 40
+
+# define VOID ' '
+# define ASTEROID '#'
+# define PLAYER 'A'
+# define ENEMY 'T'
+# define PLAYER_MISSILE '\''
+# define ENEMY_MISSILE '*'
+
+// GAME STATES
+
+# define ERROR -1
+# define END 0
+# define MENU 1
+# define PLAY 2
+# define GAME_OVER 3
+
+// STATS CONFIGS
+
+# define STAT_WIDTH 20
+# define STAT_HEIGHT 4
+# define STAT_MARGIN 3
+# define STAT_PADDING 1
+
+// HEALTH CONFIGS
+
+# define HEALTH_WIDTH 11
+# define HEALTH_MARGIN 3
+# define HEALTH_PADDING 1
+# define HEART_HEIGHT 7
+
+// DIFFICULTY CONFIG
+# define PLAYER_COOLDOWN 20
+# define FPS 60;
+# define ASTEROID_SPAWN_RATE 1 // divided by 1000
+
+#endif
